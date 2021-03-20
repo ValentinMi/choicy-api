29990da6cfced515ce106af13bc1c9ef1ae1ea32
Valentin Misiaszek <mowerv6@gmail.com>
2021-03-21 00:33:34 +0100

    feat(db): connect to dev db on dev env

--- src/startup/db.ts
+++ src/startup/db.ts
@@ -2,9 +2,12 @@ import mongoose from "mongoose";
 import * as dotenv from "dotenv";
 dotenv.config();
 
+const DB_NAME =
+  process.env.ENV === "PROD" ? process.env.DB_NAME : process.env.DB_DEV_NAME;
+
 const connectDb = () => {
   try {
-    const db = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.tftlk.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+    const db = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.tftlk.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`;
     mongoose.connect(db, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
