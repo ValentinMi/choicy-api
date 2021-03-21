d8792a10c508a2bd039de95836a510aa27712285
Valentin Misiaszek <mowerv6@gmail.com>
2021-03-21 01:48:54 +0100

    feat(auth): rework auth validation

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,16 +1,27 @@
 import express from "express";
 import bcrypt from "bcrypt";
-import { User, validateUser as validate } from "../models/user";
+import { User } from "../models/user";
 import { handleApiError } from "../utils/handleApiError";
+import Joi from "joi";
+import { IUser } from "src/types";
 const router = express.Router();
+
+export const validateAuth = (user: IUser) => {
+  const schema = Joi.object({
+    email: Joi.string().email().min(5).max(100).required(),
+    password: Joi.string().min(8).max(255).required(),
+  });
+
+  return schema.validate(user);
+};
 
 // Login
 router.post("/", async (req, res) => {
   try {
-    const { error } = validate(req.body);
+    const { error } = validateAuth(req.body);
     if (error) return res.status(400).send(error.details[0].message);
 
-    const user = await User.findOne({ username: req.body.username });
+    const user = await User.findOne({ email: req.body.email });
     const validPassword = await bcrypt.compare(
       req.body.password,
       user!.password!
